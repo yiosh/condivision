0a179dc8932c7bac0e04598d302734d5624592a5
Arkango <arcangelo@debian>
2018-01-24 17:09:31 +0000

    prima versione

---
+++ banqueting/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+# Create your views here.
+
+def index(request):
+    context = {}
+    return render(request, 'banqueting/index.html', context)
+
+
+def modulo(request):
+    context = {placeholder:'value'}
+    return render(request, 'banqueting/modulo.html', context)
